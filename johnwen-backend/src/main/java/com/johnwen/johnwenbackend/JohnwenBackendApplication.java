f72b50d5b423a5c717c15c95054c8de9cf239e9b
unknown <jem0101@auburn.edu>
2023-01-31 19:52:54 -0600

    adding initial spring project

---
+++ johnwen-backend/src/main/java/com/johnwen/johnwenbackend/JohnwenBackendApplication.java
@@ -0,0 +1,13 @@
+package com.johnwen.johnwenbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JohnwenBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JohnwenBackendApplication.class, args);
+	}
+
+}
---
+++ johnwen-backend/src/test/java/com/johnwen/johnwenbackend/JohnwenBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.johnwen.johnwenbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JohnwenBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
