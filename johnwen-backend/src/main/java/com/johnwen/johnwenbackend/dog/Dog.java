87dcd3380ab170baff4393b11425ea58082a3e34
Yuj02 <Yuj2502@gmail.com>
2023-02-02 17:34:07 -0600

    Added CORS policy to allow request from React to API and set up request using Axios

--- johnwen-backend/src/main/java/com/johnwen/johnwenbackend/dog/Dog.java
+++ johnwen-backend/src/main/java/com/johnwen/johnwenbackend/dog/Dog.java
@@ -1,4 +1,68 @@
 package com.johnwen.johnwenbackend.dog;
 
+import java.time.LocalDate;
 public class Dog {
+    private Long id;
+    private String name;
+    private LocalDate dob;
+    private Integer age;
+
+    public Dog() {
+    }
+
+    public Dog(Long id, String name, LocalDate dob, Integer age) {
+        this.id = id;
+        this.name = name;
+        this.dob = dob;
+        this.age = age;
+    }
+
+    // one without id because database generates it.
+    public Dog(String name, LocalDate dob, Integer age) {
+        this.name = name;
+        this.dob = dob;
+        this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Dog{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", dob=" + dob +
+                ", age=" + age +
+                '}';
+    }
 }
--- johnwen-backend/src/main/java/com/johnwen/johnwenbackend/dog/DogController.java
+++ johnwen-backend/src/main/java/com/johnwen/johnwenbackend/dog/DogController.java
@@ -1,14 +1,39 @@
 package com.johnwen.johnwenbackend.dog;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping(path = "/home")
 public class DogController {
     @GetMapping
-    public String bark() {
-        return "Woof!";
+    public List<Dog> getDog() {
+        return List.of(
+                new Dog(
+                        1L,
+                        "Rice",
+                        LocalDate.of(2022, Month.APRIL,23),
+                        0
+                ),
+                new Dog(
+                        2L,
+                        "Happy",
+                        LocalDate.of(2022, Month.MARCH, 1),
+                        0
+                ),
+                new Dog(
+                        2L,
+                        "Dogge",
+                        LocalDate.of(2022, Month.DECEMBER, 5),
+                        0
+                )
+        );
     }
 }
